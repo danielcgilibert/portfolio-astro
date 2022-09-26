ad6195f3339163d521525f764d68f24fc73f9155
DanielCG <danielcgilibert@gmail.com>
2022-09-26 12:08:26 +0200

    :bento: ADD data

---
+++ src/data/projects.js
@@ -0,0 +1,45 @@
+export const proyects = [
+  {
+    cover: 'filmoteca',
+    name: 'Filmoteca',
+    desc: 'Aplicación de películas con llamadas a distintas API, construida con firebase y react',
+    hrefCode: 'https://github.com/danielcgilibert/Filmoteca',
+    hrefWeb: 'https://filmoteca-994ba.web.app/',
+  },
+  {
+    cover: 'fiber',
+    name: 'Fiber',
+    desc: 'Landing Page',
+    hrefCode: 'https://github.com/danielcgilibert/Fiber-Landing-Page',
+    hrefWeb: 'https://danielcgilibert.github.io/Fiber-Landing-Page/',
+  },
+  {
+    cover: 'otherPortfolio',
+    name: 'Portfolio',
+    desc: 'Antiguo portfolio',
+    hrefCode: 'https://github.com/danielcgilibert/Fiber-Landing-Page',
+    hrefWeb: 'https://danielcgilibert.github.io/Fiber-Landing-Page/',
+  },
+  {
+    cover: 'chart',
+    name: 'Chart',
+    desc: 'Gráfica dinamica',
+    hrefCode: 'https://github.com/danielcgilibert/Fiber-Landing-Page',
+    hrefWeb: 'https://danielcgilibert.github.io/Fiber-Landing-Page/',
+  },
+  {
+    cover: 'grid',
+    name: 'Grid ',
+    desc: 'Gráfica dinamica',
+    hrefCode: 'https://github.com/danielcgilibert/Fiber-Landing-Page',
+    hrefWeb: 'https://danielcgilibert.github.io/Fiber-Landing-Page/',
+  },
+
+  {
+    cover: 'product',
+    name: 'Grid ',
+    desc: 'Gráfica dinamica',
+    hrefCode: 'https://github.com/danielcgilibert/Fiber-Landing-Page',
+    hrefWeb: 'https://danielcgilibert.github.io/Fiber-Landing-Page/',
+  },
+]
