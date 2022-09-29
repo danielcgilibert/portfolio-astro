4df1a508c83f60c85293b29cba22df18e0db238c
DanielCG <danielcgilibert@gmail.com>
2022-09-29 10:38:24 +0200

    :recycle: Refactor code

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,45 +1,45 @@
 export const proyects = [
-  {
-    cover: 'filmoteca',
-    name: 'Filmoteca',
-    desc: 'Aplicación de películas con llamadas a distintas API, construida con firebase y react',
-    hrefCode: 'https://github.com/danielcgilibert/Filmoteca',
-    hrefWeb: 'https://filmoteca-994ba.web.app/',
-  },
-  {
-    cover: 'fiber',
-    name: 'Fiber',
-    desc: 'Landing Page',
-    hrefCode: 'https://github.com/danielcgilibert/Fiber-Landing-Page',
-    hrefWeb: 'https://danielcgilibert.github.io/Fiber-Landing-Page/',
-  },
-  {
-    cover: 'otherPortfolio',
-    name: 'Portfolio',
-    desc: 'Antiguo portfolio',
-    hrefCode: 'https://github.com/danielcgilibert/Fiber-Landing-Page',
-    hrefWeb: 'https://danielcgilibert.github.io/Fiber-Landing-Page/',
-  },
-  {
-    cover: 'chart',
-    name: 'Chart',
-    desc: 'Gráfica dinamica',
-    hrefCode: 'https://github.com/danielcgilibert/Fiber-Landing-Page',
-    hrefWeb: 'https://danielcgilibert.github.io/Fiber-Landing-Page/',
-  },
-  {
-    cover: 'grid',
-    name: 'Layout Grid',
-    desc: 'Gráfica dinamica',
-    hrefCode: 'https://github.com/danielcgilibert/Fiber-Landing-Page',
-    hrefWeb: 'https://danielcgilibert.github.io/Fiber-Landing-Page/',
-  },
+	{
+		cover: 'filmoteca',
+		name: 'Filmoteca',
+		desc: 'Aplicación de películas con llamadas a distintas API, construida con firebase y react',
+		hrefCode: 'https://github.com/danielcgilibert/Filmoteca',
+		hrefWeb: 'https://filmoteca-994ba.web.app/'
+	},
+	{
+		cover: 'fiber',
+		name: 'Fiber',
+		desc: 'Landing Page',
+		hrefCode: 'https://github.com/danielcgilibert/Fiber-Landing-Page',
+		hrefWeb: 'https://danielcgilibert.github.io/Fiber-Landing-Page/'
+	},
+	{
+		cover: 'otherPortfolio',
+		name: 'Portfolio',
+		desc: 'Antiguo portfolio',
+		hrefCode: 'https://github.com/danielcgilibert/Fiber-Landing-Page',
+		hrefWeb: 'https://danielcgilibert.github.io/Fiber-Landing-Page/'
+	},
+	{
+		cover: 'chart',
+		name: 'Chart',
+		desc: 'Gráfica dinamica',
+		hrefCode: 'https://github.com/danielcgilibert/Fiber-Landing-Page',
+		hrefWeb: 'https://danielcgilibert.github.io/Fiber-Landing-Page/'
+	},
+	{
+		cover: 'grid',
+		name: 'Layout Grid',
+		desc: 'Gráfica dinamica',
+		hrefCode: 'https://github.com/danielcgilibert/Fiber-Landing-Page',
+		hrefWeb: 'https://danielcgilibert.github.io/Fiber-Landing-Page/'
+	},
 
-  {
-    cover: 'product',
-    name: 'product',
-    desc: 'Gráfica dinamica',
-    hrefCode: 'https://github.com/danielcgilibert/Fiber-Landing-Page',
-    hrefWeb: 'https://danielcgilibert.github.io/Fiber-Landing-Page/',
-  },
+	{
+		cover: 'product',
+		name: 'product',
+		desc: 'Gráfica dinamica',
+		hrefCode: 'https://github.com/danielcgilibert/Fiber-Landing-Page',
+		hrefWeb: 'https://danielcgilibert.github.io/Fiber-Landing-Page/'
+	}
 ]
