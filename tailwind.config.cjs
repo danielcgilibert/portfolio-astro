63f368d5d6307ce0856e79ebf7d6cec7dcc3b2a6
DanielCG <danielcgilibert@gmail.com>
2022-09-30 10:47:45 +0200

    :wrench: chore: Configured tailwindcss/typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,21 +2,21 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Helvetica Neue', ...defaultTheme.fontFamily.sans],
-      },
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Helvetica Neue', ...defaultTheme.fontFamily.sans]
+			},
 
-      backgroundColor: {
-        dark: '#1E1E1E',
-      },
-      colors: {
-        yellow: '#FDE48B',
-        gray: '#888888',
-      },
-    },
-  },
-  plugins: [],
+			backgroundColor: {
+				dark: '#1E1E1E'
+			},
+			colors: {
+				yellow: '#FDE48B',
+				gray: '#888888'
+			}
+		}
+	},
+	plugins: [require('@tailwindcss/typography')]
 }
