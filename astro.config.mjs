04aab13505551efc18fc915cc63f791bbc9735ac
DanielCG <danielcgilibert@gmail.com>
2022-10-08 18:42:07 +0200

    :wrench: Chore: update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
-  site: 'https://danielcgilibert.github.io',
-  base: '/portfolio-astro',
+	integrations: [tailwind(), image()],
+	site: 'https://danielcgilibert.github.io',
+	base: '/'
 })
